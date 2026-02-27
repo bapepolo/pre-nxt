9fbfb3c296bd1110fab763bf8ae444174263999b
bapepolo <kwonsoonbeom@kaist.ac.kr>
2026-02-27 23:54:46 +0900

    hotfix: recover 'currentColor' and add 'width' and 'height' at darkModeIcon.tsx for the safari environment.

--- src/icons/darkModeIcon.tsx
+++ src/icons/darkModeIcon.tsx
@@ -1,15 +1,14 @@
 function MoonIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M21 12.5A8.5 8.5 0 1111.5 3 
-              6.5 6.5 0 0021 12.5z"/>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M21 12.5A8.5 8.5 0 1111.5 3 6.5 6.5 0 0021 12.5z"/>
     </svg>
   );
 }
 
 function SunIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
       <circle cx="12" cy="12" r="4"/>
       <line x1="12" y1="2" x2="12" y2="5"/>
       <line x1="12" y1="19" x2="12" y2="22"/>
@@ -25,11 +24,15 @@ function SunIcon() {
 
 function SystemIcon() {
   return (
-    <svg viewBox="0 0 24 24"
-        fill="none"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round">
+    <svg
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
 
       {/* monitor frame */}
       <rect x="3" y="4" width="18" height="12" rx="2"/>
